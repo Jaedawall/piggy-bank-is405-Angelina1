02859db1745331baab9f246bea9c41c413a7ca53
Jaewall <jaedawaldram@gmail.com>
2023-10-30 11:28:42 -0600

    Added the navigation bar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import SavingsTracker from "./SavingsTracker";
+
+function App() {
+  return (
+    <div className="App">
+      <SavingsTracker />
+    </div>
+  );
+}
+
+export default App;
